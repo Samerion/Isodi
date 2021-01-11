e99ee1516531f12b0b2690845c9ac2b1aaad1055
Soaku <dominik@drozak.net>
2021-01-11 11:56:23 +0100

    Bugfix decoration rotation not working properly
    This bug was so stupid, how did I even write it this way lmao

--- source/isodi/raylib/resources/decoration.d
+++ source/isodi/raylib/resources/decoration.d
@@ -116,8 +116,9 @@ struct Decoration {
 
                 import std.conv : to;
 
-                const angleDelimiter = 360 / deco.options.angles  +  deco.direction;
-                const angle = to!uint(cell.display.camera.angle.x / angleDelimiter  +  0.5);
+                const angleDelimiter = 360 / deco.options.angles;
+                const rotation = cell.display.camera.angle.x + deco.direction;
+                const angle = to!uint(rotation / angleDelimiter  +  0.5);
 
                 const atlasWidth = deco.texture.width / deco.options.angles;
                 const tileScale = cell.tile.scale;
